9b1f348db61b66f726591dfeed32562d5491273e
Vornekz <6319553@gmai.com>
2022-01-04 16:56:51 +0200

    Text update

--- src/components/D3_Graphic2.vue
+++ src/components/D3_Graphic2.vue
@@ -4,7 +4,7 @@
       <svg id="svg2"
            :width="width"
            :height="height"
-           :viewBox="`0, 0, ${width}, ${height}`">
+           :viewBox="`0, -20, ${width + 200}, ${height + 100}`">
         <g></g>
       </svg>
     </div>
@@ -28,7 +28,7 @@ export default class D3_Graphic2 extends Vue {
   private height: number = 1100;
   private radius: number = 6;
   private groupOrder: string[] = ["region", "subregion"];
-  private h: HierarchyNode<any> = d3.hierarchy({});
+  private hierarchy: HierarchyNode<any> = d3.hierarchy({});
 
   get layout() {
     return d3.tree()
@@ -40,15 +40,15 @@ export default class D3_Graphic2 extends Vue {
   }
 
   createHierarchy(val: any) {
-    const h = d3.hierarchy(val, (v) => v[1])
+    const hierarchy = d3.hierarchy(val, (v) => v[1])
 
-    h.sum(v => v.population).value
+    hierarchy.sum(v => v.population).value
 
-    h.sort((a, b) => d3.ascending(b.value, a.value))
+    hierarchy.sort((a, b) => d3.ascending(b.value, a.value))
 
-    this.layout(h)
+    this.layout(hierarchy)
 
-    this.h = h;
+    this.hierarchy = hierarchy;
 
     this.createLinks()
     this.createCircle()
@@ -58,136 +58,139 @@ export default class D3_Graphic2 extends Vue {
   createLinks() {
     d3.select("#svg2")
         .select("g")
-        .append("g")
-          .attr("fill", "none")
-        .selectAll("path")
-          .data(this.h.links())
-          .enter().append("path")
-            .attr("d", (d: any) => {
-              return d3.linkHorizontal()({
-                source: [d.source.y, d.source.x],
-                target: d.target.children == null ? [d.target.y - 140, d.target.x] : [d.target.y, d.target.x]
-              })
-            })
-            .attr("stroke", "#c44141")
-            .attr("stroke-width", "3")
-        .on("mouseover", (event, d: any) => {
-          if (d.target.children == null) {
-            d3.select(event.currentTarget)
-                .attr("stroke", "#a8c441")
-
-            d3.select("#svg2")
-                .selectAll(`text`)
-                  .filter(function():boolean {
-                    return d3.select(this).text() == d.target.data.key
+          .append("g")
+            .attr("fill", "none")
+            .selectAll("path")
+              .data(this.hierarchy.links())
+              .enter()
+                .append("path")
+                .attr("d", (d: any) => {
+                  return d3.linkHorizontal()({
+                    source: [d.source.y, d.source.x],
+                    target: [d.target.y, d.target.x]
                   })
-                  .attr("fill-opacity", "1")
-
-            d3.select("#svg2")
-                .selectAll(`text`)
-                .filter(function():boolean {
-                  return d3.select(this).text() == d.target.value + " " + "people"
                 })
-                .attr("fill-opacity", "1")
-          }
-        })
-        .on("mouseout", (event, d: any) => {
-          if (d.target.children == null) {
-            d3.select(event.currentTarget)
                 .attr("stroke", "#c44141")
+                .attr("stroke-width", "3")
+                .on("mouseover", (event, d: any) => {
+                  if (d.target.children == null) {
+                    d3.select(event.currentTarget)
+                        .attr("stroke", "#a8c441")
 
-            d3.select("#svg2")
-                .selectAll(`text`)
-                  .filter(function():boolean {
-                    return d3.select(this).text() == d.target.data.key
-                  })
-                  .attr("fill-opacity", "0")
+                    d3.select("#svg2")
+                        .selectAll(`text`)
+                          .filter(function():boolean {
+                            return d3.select(this).text() == d.target.data.key
+                          })
+                          .attr("fill-opacity", "1")
 
-            d3.select("#svg2")
-                .selectAll(`text`)
-                .filter(function():boolean {
-                  return d3.select(this).text() == d.target.value + " " + "people"
+                    d3.select("#svg2")
+                        .selectAll(`text`)
+                        .filter(function():boolean {
+                          return d3.select(this).text() == d.target.value  + " " + "people"
+                        })
+                        .attr("fill-opacity", "1")
+                  }
                 })
-                .attr("fill-opacity", "0")
-          }
-        })
+                .on("mouseout", (event, d: any) => {
+                  if (d.target.children == null) {
+                    d3.select(event.currentTarget)
+                        .attr("stroke", "#c44141")
+
+                    d3.select("#svg2")
+                        .selectAll(`text`)
+                          .filter(function():boolean {
+                            return d3.select(this).text() == d.target.data.key
+                          })
+                          .attr("fill-opacity", "0")
+
+                    d3.select("#svg2")
+                        .selectAll(`text`)
+                        .filter(function():boolean {
+                          return d3.select(this).text() == d.target.value  + " " + "people"
+                        })
+                        .attr("fill-opacity", "0")
+                  }
+                })
   }
 
   createCircle() {
     d3.select("#svg2")
         .select("g")
-        .append("g")
-        .selectAll("circle")
-        .data(this.h.descendants())
-        .enter()
-          .append("circle")
-            .attr("transform", (d: any) => {
-              return `translate(${d.y + this.radius},${d.x})`
-            })
-            .attr("r", this.radius)
-            .attr("fill", "#fff")
-            .attr("fill-opacity",(d: any) => d.children == null ? "0" : "1")
-            .attr("cursor", "pointer")
-            .on("mouseover", (event, d: any) => {
-                d3.select(event.currentTarget)
-                    .attr("fill", "#a8c441")
+          .append("g")
+            .selectAll("circle")
+            .data(this.hierarchy.descendants())
+            .enter()
+              .append("circle")
+                .attr("transform", (d: any) => {
+                  return `translate(${d.y + this.radius}, ${d.x})`
+                })
+                .attr("r", (d: any) => d.children != null ? `${this.radius}`: "0")
+                .attr("fill", "#fff")
+                .attr("fill-opacity",(d: any) => d.children == null ? "0" : "1")
+                .attr("cursor", "pointer")
+                .on("mouseover", (event, d: any) => {
+                    d3.select(event.currentTarget)
+                        .attr("fill", "#a8c441")
 
-                d3.select("#svg2")
-                    .selectAll(`text`)
-                    .filter(function():boolean {
-                      return d3.select(this).text() == d.value + " " + "people"
-                    })
-                    .attr("fill-opacity", "1")
-            })
-            .on("mouseout", (event, d: any) => {
-                d3.select(event.currentTarget)
-                    .attr("fill", "#fff")
+                    d3.select("#svg2")
+                        .selectAll(`text`)
+                        .filter(function():boolean {
+                          return d3.select(this).text() == (d.value * 0.000001).toFixed(2) + "m " + "people"
+                        })
+                        .attr("fill-opacity", "1")
 
-                d3.select("#svg2")
-                    .selectAll(`text`)
-                    .filter(function():boolean {
-                      return d3.select(this).text() == d.value + " " + "people"
-                    })
-                    .attr("fill-opacity", "0")
-            })
+                })
+                .on("mouseout", (event, d: any) => {
+                    d3.select(event.currentTarget)
+                        .attr("fill", "#fff")
+
+                    d3.select("#svg2")
+                        .selectAll(`text`)
+                        .filter(function():boolean {
+                          return d3.select(this).text() == (d.value * 0.000001).toFixed(2) + "m " + "people"
+                        })
+                        .attr("fill-opacity", "0")
+                })
   }
 
   createText() {
     d3.select("#svg2")
         .select("g")
-        .append("g")
-        .selectAll("text")
-        .data(this.h.descendants())
-        .enter()
-          .append("text")
-            .attr("transform", (d: any) => {
-               return d.children == null ? `translate(${d.y - 130},${d.x + 10})` : `translate(${d.y},${d.x - 10})`
-            })
-            .text((d: any) => d.data[0]? d.data[0]: d.data.key)
-              .attr("fill", "#c1c1d0")
-              .attr("fill-opacity", (d: any) => d.children == null ? "0" : "1")
-              .attr("font-size", "13px")
+          .append("g")
+            .selectAll("text")
+            .data(this.hierarchy.descendants())
+            .enter()
+              .append("text")
+                .attr("transform", (d: any) => {
+                   return d.children == null ? `translate(${d.y + 10},${d.x + 10})` : `translate(${d.y},${d.x - 10})`
+                })
+                .text((d: any) => d.data[0]? d.data[0]: d.data.key)
+                  .attr("fill", "#c1c1d0")
+                  .attr("fill-opacity", (d: any) => d.children == null ? "0" : "1")
+                  .attr("font-size", "15px")
+                  .style("user-select", "none")
 
     d3.select("#svg2")
         .select("g")
-        .append("g")
-        .selectAll("text")
-        .data(this.h.descendants())
-        .enter()
-          .append("text")
-          .attr("transform", (d: any) => {
-            return d.parent == null ? `translate(${d.y},${d.x - 30})` : d.children == null ? `translate(${d.y - 130},${d.x + 25})` : `translate(${d.y - 102},${d.x - 10})`
-          })
-            .text((d: any) => d.value + " " + "people")
-              .attr("fill", "#c1c1d0")
-              .attr("fill-opacity", "0")
-              .attr("font-size", "12px")
+          .append("g")
+            .selectAll("text")
+            .data(this.hierarchy.descendants())
+            .enter()
+              .append("text")
+              .attr("transform", (d: any) => {
+                return d.parent == null ? `translate(${d.y},${d.x - 30})` : d.children == null ? `translate(${d.y + 10},${d.x + 25})` : `translate(${d.y - 115},${d.x - 10})`
+              })
+                .text((d: any) =>d.children == null ? d.value + " " + "people" : (d.value * 0.000001).toFixed(2) + "m " + "people")
+                  .attr("fill", "#c1c1d0")
+                  .attr("fill-opacity", "0")
+                  .attr("font-size", "15px")
+                  .style("user-select", "none")
   }
 
   @Watch("groupData")
   onGroupDataChanged(val: any[]) {
     this.createHierarchy(val)
-    console.log(this.h.links())
   }
 
 }
@@ -196,6 +199,7 @@ export default class D3_Graphic2 extends Vue {
 <style lang="scss" scoped>
 #graph2 {
   display: flex;
+  position: relative;
   justify-content: center;
 }
 </style>
